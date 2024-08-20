550493aa4222604836f50bde168398e5bbcc5862
Rahma-salhy <rahmasalhi2112@gmail.com>
2024-08-20 13:58:15 +0100

    finished interface

--- project.js
+++ project.js
@@ -1,3 +1,10 @@
-var push=$('#submit')
-push.css({'display':'flex','justify-content':'center'})
-$('body').append(push)
+index = 0%
+function submit() {
+
+
+  if (index === images.length) {
+    index = 0
+  }
+  $('img').attr("src", images[++index])
+}
+$('#button').on('click', submit)

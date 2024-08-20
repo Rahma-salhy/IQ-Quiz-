99045dde7d770b3ef4a3db70e68eefe6697b63c4
Rahma-salhy <rahmasalhi2112@gmail.com>
2024-08-20 23:34:41 +0100

    finished with fiew mistakes

--- project.js
+++ project.js
@@ -1,10 +1,27 @@
-index = 0%
-function submit() {
+$('#submit').click(function() {
+    window.location.href = 'pege2.html';
+})
+
+$(document).ready(function(){
+    var correctAnswers=["10","snake","Animal","E","4","12","Appel","Enveloppe","3","Anxious"]
+    $('#submit').click(function(){
+        var score=0
+        for(var i =0 ;i<correctAnswers.length;i++){
+            var checkedAnswers=$('input[name="q' +(i+1)+ '"]:checked').val()
+            if (checkedAnswers===correctAnswers[i]) {
+                console.log(score);
+                
+                score=score+10
+            }
+        }
+
+        
+    })
+   
+})
+$('#score').text('YOU SCORE IS:' + score +'%')
 
 
-  if (index === images.length) {
-    index = 0
-  }
-  $('img').attr("src", images[++index])
-}
-$('#button').on('click', submit)
+
+
+

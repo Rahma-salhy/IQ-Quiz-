39bfd3faddd54fec610e7bada1a0a08299263095
Rahma-salhy <rahmasalhi2112@gmail.com>
2024-08-21 08:55:21 +0100

    finished

--- project.js
+++ project.js
@@ -1,25 +1,26 @@
-$('#submit').click(function() {
-    window.location.href = 'pege2.html';
-})
 
 $(document).ready(function(){
     var correctAnswers=["10","snake","Animal","E","4","12","Appel","Enveloppe","3","Anxious"]
-    $('#submit').click(function(){
+    $('#sumit').click(function(){
         var score=0
         for(var i =0 ;i<correctAnswers.length;i++){
             var checkedAnswers=$('input[name="q' +(i+1)+ '"]:checked').val()
+            console.log(checkedAnswers);
+            
             if (checkedAnswers===correctAnswers[i]) {
-                console.log(score);
                 
                 score=score+10
             }
         }
 
+        console.log(score);
+        $('#score').text('YOU SCORE IS:' + score +'%')
         
     })
-   
+    $('#a').show()
 })
-$('#score').text('YOU SCORE IS:' + score +'%')
+
+
 
 
 
